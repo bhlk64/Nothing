0bca42da7ecbdbe8fc0ebf4130d505a1439a703e
bhlk64 <150582425+bhlk64@users.noreply.github.com>
2025-06-03 12:50:06 +0700

    Remove merge BHopKeyboard and BHopGui

--- OverhaulEvade.lua
+++ OverhaulEvade.lua
@@ -1,4 +1,4 @@
-local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+staffwatch (Notify if a staff member of the game joins the server)local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/bhlk64/Nothing/refs/heads/main/InterfaceManager.lua"))()
 
